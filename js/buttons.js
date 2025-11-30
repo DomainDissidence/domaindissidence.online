aeba85d155f14319718703894615f95c0fbc1329
Ryn. <rynstwrt17@gmail.com>
2025-11-29 18:56:51 -0600

    Update

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,12 @@
-document.querySelectorAll("#buttons button").forEach(btn => {
-    btn.addEventListener("click", () => window.alert("Coming soon!"));
-});
+const DOWNLOAD_URL = "https://github.com/rynstwrt/DomainDissidence/releases";
+const USAGE_URL = "https://github.com/rynstwrt/DomainDissidence/wiki";
+
+const downloadButton = document.querySelector("#download-button");
+const usageButton = document.querySelector("#usage-button");
+
+downloadButton.addEventListener("click", () =>
+    window.open(DOWNLOAD_URL, "_blank"));
+
+
+usageButton.addEventListener("click", () =>
+    window.open(USAGE_URL, "_blank"));
