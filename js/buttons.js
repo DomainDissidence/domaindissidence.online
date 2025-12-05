1904e45cb0786fb9c11e71bc38dd881b4f335e3f
Ryn. <rynstwrt17@gmail.com>
2025-12-04 20:02:48 -0600

    Backup

--- js/buttons.js
+++ js/buttons.js
@@ -1,13 +1,13 @@
-const DOWNLOAD_URL = "https://github.com/rynstwrt/DomainDissidence/releases";
-const USAGE_URL = "https://github.com/rynstwrt/DomainDissidence/wiki";
-
-
-const aboutSection = document.querySelector("#about");
-document.querySelector("#learn-more-button").addEventListener("click", () =>
-    aboutSection.scrollIntoView({ behavior: "smooth"}));
-
-document.querySelector("#download-button").addEventListener("click", () =>
-    window.open(DOWNLOAD_URL, "_blank"));
-
-document.querySelector("#usage-button").addEventListener("click", () =>
-    window.open(USAGE_URL, "_blank"));
+// const DOWNLOAD_URL = "https://github.com/rynstwrt/DomainDissidence/releases";
+// const USAGE_URL = "https://github.com/rynstwrt/DomainDissidence/wiki";
+//
+//
+// const aboutSection = document.querySelector("#about");
+// document.querySelector("#learn-more-button").addEventListener("click", () =>
+//     aboutSection.scrollIntoView({ behavior: "smooth"}));
+//
+// document.querySelector("#download-button").addEventListener("click", () =>
+//     window.open(DOWNLOAD_URL, "_blank"));
+//
+// document.querySelector("#usage-button").addEventListener("click", () =>
+//     window.open(USAGE_URL, "_blank"));
