91ee7175e4f6031d0b3ba4abe6a3b61be7766ecc
Ryn. <rynstwrt17@gmail.com>
2025-12-09 14:34:57 -0600

    Update links

--- js/buttons.js
+++ js/buttons.js
@@ -1,20 +1,5 @@
-const COMING_SOON_MODE = false;
+const INSTALL_URL = "https://github.com/rynstwrt/DomainDissidence/blob/main/INSTALL.md";
+const SOURCE_URL = "https://github.com/rynstwrt/DomainDissidence";
 
-const CREATE_WEBSITE_URL = "https://github.com/rynstwrt/DomainDissidence";
-const GUIDE_URL = "https://github.com/rynstwrt/DomainDissidence";
-
-
-let buttonIdUrlDict = {
-    "create-website-button": CREATE_WEBSITE_URL,
-    "guide-button": GUIDE_URL
-};
-
-
-document.querySelectorAll("#landing button")
-    .forEach(btn => btn.addEventListener("click", () => {
-        if (COMING_SOON_MODE)
-            return window.alert("Coming soon...");
-
-        const url = buttonIdUrlDict[btn.id];
-        url && window.open(url, "_blank");
-    }));
+document.querySelector("#usage-button").addEventListener("click", () => window.open(INSTALL_URL));
+document.querySelector("#source-button").addEventListener("click", () => window.open(SOURCE_URL));
