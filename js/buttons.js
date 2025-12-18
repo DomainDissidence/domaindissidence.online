1984b9ea42fc281c818d35faaa88999ccf5fe196
Ryn. <rynstwrt17@gmail.com>
2025-12-18 05:07:46 -0600

    Update links

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,5 @@
-const INSTALL_URL = "https://github.com/rynstwrt/DomainDissidence/blob/main/INSTALL.md";
-const SOURCE_URL = "https://github.com/rynstwrt/DomainDissidence";
+const INSTALL_URL = "https://github.com/DomainDissidence/DomainDissidence/blob/main/INSTALL.md";
+const SOURCE_URL = "https://github.com/DomainDissidence/DomainDissidence";
 
 document.querySelector("#usage-button").addEventListener("click", () => window.open(INSTALL_URL));
 document.querySelector("#source-button").addEventListener("click", () => window.open(SOURCE_URL));
